f95d6c208bcc4468cbe43cfe441f7b36a91634b4
SEJ <sej@needrent.dk>
2019-09-03 11:52:29 +0200

    loops

---
+++ loop.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/*let counter;
+for (counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+
+console.log(`we are a count of ${counter}`);*/
+
+/*for (let counter = 0; counter < 10; counter++) {
+  console.log(`This counts 0 to 9 | ${counter}`);
+}
+
+for (let counter = 1; counter < 11; counter++) {
+  console.log(`This counts 1 to 10 | ${counter}`);
+}
+
+for (let counter = 10; counter >= 0; counter--) {
+  if (counter == 0) {
+    console.log(counter);
+    console.log("liftoff");
+  } else {
+    console.log(counter);
+  }
+}
+//Only shows uneven numbers
+for (let i = 1; i <= 19; i++) {
+  if (i % 2 == 0) {
+    // % tells the code that is had to be able to divide i with 2 (even numbers) so added the ==0 tells the code that the number should NOT be even
+    continue; // if the number uneven then continue, oftewise skip
+  }
+  console.log(i);
+
+for (let i = 1; i <= 16777216; i *= 2) {
+  console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}*/
+for (let i = 111; i <= 138; i += 3) {
+  console.log(i);
+}
